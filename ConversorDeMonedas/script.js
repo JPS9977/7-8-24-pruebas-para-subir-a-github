ba2f2be240ec09324eef28e49f522228531faa8d
JPS9977 <juampy9977@gmail.com>
2024-08-07 13:16:59 -0300

    .

--- ConversorDeMonedas/script.js
+++ ConversorDeMonedas/script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const apiKey = '31d6499ea660fcc7c2cc9a3e';  // Reemplaza con tu propia API key de ExchangeRate-API.
     const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 
-    // Cargar la lista de monedas
     fetch(apiURL)
         .then(response => response.json())
         .then(data => {
@@ -23,12 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 fromCurrency.appendChild(option1);
                 toCurrency.appendChild(option2);
             });
-        })
-        .catch(error => {
-            console.error('Error fetching currency data:', error);
         });
 
-    // Manejar el evento de conversión
     convertButton.addEventListener('click', () => {
         const from = fromCurrency.value;
         const to = toCurrency.value;
@@ -38,18 +33,11 @@ document.addEventListener('DOMContentLoaded', () => {
             fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${from}/${to}/${amountValue}`)
                 .then(response => response.json())
                 .then(data => {
-                    if (data.result === "success") {
-                        result.textContent = `${amountValue} ${from} = ${data.conversion_result} ${to}`;
-                    } else {
-                        result.textContent = 'Error en la conversión. Por favor, intenta de nuevo.';
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching conversion data:', error);
-                    result.textContent = 'Error al obtener datos de conversión. Por favor, intenta de nuevo.';
+                    result.textContent = `${amountValue} ${from} = ${data.conversion_result} ${to}`;
                 });
         } else {
             result.textContent = 'Por favor, completa todos los campos.';
         }
     });
 });
+
